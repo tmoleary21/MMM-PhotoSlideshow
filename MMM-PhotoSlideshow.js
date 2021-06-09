82a53a017d6f578dda2910c4cb126663e7052858
Tyson O'Leary <olearytyson@gmail.com>
2021-06-09 00:49:20 -0600

    Node helper communication and photo change functions

--- MMM-PhotoSlideshow.js
+++ MMM-PhotoSlideshow.js
@@ -1,12 +1,51 @@
 Module.register("MMM-PhotoSlideshow", {
 
 	defaults: {
-		config: {
-			albumPath: "./"
-		}
+		albumPath: "./",
+		cycleTime: 5000, //Default 5 seconds per picture
+		animationTime: 500 //Default half second animation
+	},
+
+	album: [], //array containing names of the files in the albumPath directory
+	currentPhotoIndex: 0,
+
+	nextPhoto: function() {
+		this.currentPhotoIndex = (this.currentPhotoIndex + 1) % this.album.length;
+		this.updateDom(this.config.animationTime);
+	},
+
+	previousPhoto: function() {
+		this.currentPhotoIndex = (this.currentPhotoIndex - 1) % this.album.length;
+		this.updateDom(this.config.animationTime);
+	},
+
+	start: function() {
+		//Keep in mind the dom object has not been created yet as of this function
+		this.sendSocketNotification('REFRESH_ALBUM', this);
+	},
+
+	getScripts: function() {
+		return []; //No additional scripts as of yet
+	},
+
+	getStyles: function() {
+		return []; //No styles as of yet. Maybe later
 	},
 
 	getDom: function() {
+		//Might need to be a div to work correctly. Not sure yet
+		const img = document.createElement('img');
+		img.src = this.config.albumPath + this.album[this.currentPhotoIndex];
+		return img;
+	},
+
+	notificationReceived: function(notification, payload, sender){
+		if(notification === 'DOM_OBJECTS_CREATED'){ //Received when all dom objects from all modules are loaded
+			setInterval()
+		}
+	},
+
+	socketNotificationReceived: function(notification, payload) {
 
 	}
 });
--- node_helper.js
+++ node_helper.js
@@ -1,2 +1,29 @@
+fs = require(fs);
 const NodeHelper = require("node_helper");
-module.exports = NodeHelper.create({});
+module.exports = NodeHelper.create({
+	// Options:
+	// 1)** Have a function that creates a list of the photos in the album and send that to the core module file
+	// 2) Function that returns the next picture (keeping track of current array location) and sends that to the
+	//    core module file each time the picture changes
+
+	getUpdatedAlbum: async function(path) {
+		let album;
+		await fs.readdir(path, function(err, files){
+			if(err){
+				//error handling
+			}
+			album = files.filter(function(value, index, arr){
+				return value.indexOf('.') !== -1;
+			});
+		});
+		return album;
+	},
+
+
+
+	socketNotificationReceived: function(notification, payload) {
+		if(notification === 'REFRESH_ALBUM'){
+			payload.album = getUpdatedAlbum(payload.config.albumPath);
+		}
+	}
+});
