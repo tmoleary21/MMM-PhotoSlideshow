95228be8c540214fed617d8e1532740ea20e4963
Tyson O'Leary <olearytyson@gmail.com>
2021-06-10 22:10:07 -0600

    additional filtering for directory read

--- node_helper.js
+++ node_helper.js
@@ -7,16 +7,16 @@ module.exports = NodeHelper.create({
 	//    core module file each time the picture changes
 
 	getUpdatedAlbum: function(path) {
-		let album;
+		let album=[];
 		fs.readdirSync(path, function(err, files){
 			if(err){
 				//error handling
 			}
 			album = files.filter(function(value, index, arr){
-				return value.indexOf('.') !== -1;
+				return file.indexOf('.') !== -1 && ['jpg', 'jpeg', 'png', 'gif'].indexOf(file.split('.')[1].toLowerCase()) !== -1;
 			});
 		});
-		Log.log(album);
+		Log.log('MMM-PhotoSlideshow node_helper:  ' + album);
 		return album;
 	},
 
