4b9e963e240483427725ec6f827334ef45e6abff
Tyson O'Leary <olearytyson@gmail.com>
2021-07-05 15:16:31 -0600

    test

--- MMM-PhotoSlideshow.js
+++ MMM-PhotoSlideshow.js
@@ -74,7 +74,7 @@ Module.register("MMM-PhotoSlideshow", {
 		division.style.display = 'flex';
 		division.style.justifyContent = 'center';
 		if(this.album.length > 0) {
-			division.style.backgroundImage = 'url("./test.png")';
+			division.style.backgroundImage = 'url("./modules/MMM-PhotoSlideshow/test.png")';
 			// const img = document.createElement('img');
 			// img.src = encodeURI(this.config.albumPath + this.album[this.currentPhotoIndex]);
 			// img.class = 'photo'
